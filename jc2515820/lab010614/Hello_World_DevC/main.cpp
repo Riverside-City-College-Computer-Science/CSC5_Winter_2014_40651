54156cf31d41b01fca871f59341a8bb1362c0fa5
jasonhesperia <abc1234>
2014-01-08 19:48:52 -0800

    Signed-off-by: jasonhesperia <abc1234>

---
+++ jc2515820/lab010614/Hello_World_DevC/main.cpp
@@ -0,0 +1,21 @@
+/*
+  Jason Colburn
+  Jan 6th, 2014
+  First Program in DevC
+*/
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here!
+int main(int argc, char *argv[]) {
+    //Print Hello World
+    cout<<"Hello World"<<endl;
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
