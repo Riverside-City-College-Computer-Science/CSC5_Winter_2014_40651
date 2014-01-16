43f8ffbcafbdf56dfbd3bc3d0331bf1fa90ad715
lilj4321 <lilj4321@gmail.com>
2014-01-15 21:31:49 -0800

    Signed-off-by: lilj4321 <lilj4321@gmail.com>

---
+++ jg2194144/Lab_011514_Loops/FutureValue_WhileLoop/main.cpp
@@ -0,0 +1,46 @@
+/* 
+ * File:   main.cpp
+ * Author: Julian Gonzalez
+ * 
+ *
+ * Created on January 15, 2014, 7:14 PM
+ */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//Global Constants
+const short CNV_PERCN=100;
+//Function Prototypes
+
+//Execution Begins Here!
+int main() {
+    //Declare Variables
+    float presVal, fchrVal, iRate;
+    short nPeriods;
+    //Prompt for inputs
+    cout << "What is your present value $'s\n";
+    cin>> presVal;
+    cout << "What is your inf/inv/roi %?\n";
+    cin>> iRate;
+    iRate/=CNV_PERCN;
+    cout << "How many compounding periods? (years)\n";
+    cin>> nPeriods;
+    //setup the heading
+    cout << "Year Future Value\n";
+    cout << setprecision(2)<<fixed<<showpoint;
+    //Loop to determine the future value
+    //print every year
+    fchrVal=presVal; //at time =0
+    int year=1; //intialize before the loop
+    while (year<=nPeriods){  //the stop
+        fchrVal*=(1+iRate);
+        cout << setw(4)<< year;
+        cout << setw(11)<<fchrVal<<endl;
+        year++; //the step
+    }
+
+    return 0;
+}
+
