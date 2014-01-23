ad56e79d9d394c5ba35dc4bceb82e55f0aeb0bec
mlehrjava <mark.lehr@rcc.edu>
2014-01-22 21:34:49 -0800

    Signed-off-by: mlehrjava <mark.lehr@rcc.edu>

--- ml1150258/RockPaperScissors/main.cpp
+++ ml1150258/RockPaperScissors/main.cpp
@@ -13,20 +13,41 @@ using namespace std;
 //Global Constants
 
 //Function Prototypes
-char player();
+char choose();
 char eval(char,char);
-void tally(char,unsigned short &,unsigned short &);
+void tally(char,unsigned short &,
+        unsigned short &,unsigned short &);
 
 
 int main(int argc, char** argv) {
-
+    //Set the random number seed
+    srand(static_cast<unsigned int>(time(0)));
+    //Declare our variables;
+    char player1,player2,winner;
+    unsigned short winPly1=0,winPly2=0,nGames=100,ties=0;
+    //Loop for the number of games
+    for(int game=1;game<=nGames;game++){
+        //Let player choose
+        player1=choose();
+        player2=choose();
+        //Evaluate who won
+        winner=eval(player1,player2);
+        //Tally the results
+        tally(winner,winPly1,winPly2,ties);
+    }
+    //Output the results
+    cout<<"Player 1 won "<<winPly1<<" games"<<endl;
+    cout<<"Player 2 won "<<winPly2<<" games"<<endl;
+    cout<<"Number of tied games is "<<ties<<endl;
+    //Exit Stage Right
     return 0;
 }
 
 void tally(char win,unsigned short &wp1,
-        unsigned short &wp2){
+        unsigned short &wp2,unsigned short &ties){
     if(win=='1')wp1++;
-    if(win=='2')wp2++;
+    else if(win=='2')wp2++;
+    else ties++;
 }
 
 char eval(char p1,char p2){
@@ -45,7 +66,7 @@ char eval(char p1,char p2){
     }
 }
 
-char player(){
+char choose(){
     switch(rand()%3){
         case 0:  return 'R';
         case 1:  return 'P';
